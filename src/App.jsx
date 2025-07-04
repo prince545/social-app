f8200e64dd1d3ebe32c40017f221b1c1dcc68ce1
Simerjeet singh <daljeetkaur9570@gmail.com>
2025-07-05 02:06:45 +0530

    y

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import CreatePost from "./components/CreatePost";
-import PostList from "./components/Postlist";
+import PostList from "./components/PostList";
 import { useState, useContext } from "react";
 import PostListProvider from "./store/post-list-store";
 import AuthProvider, { AuthContext } from "./store/user-auth-store";
